07b72ee6f0218e18a6ad14509d0b1682110fe861
Yamin <Yaminosman880@gmail.com>
2026-07-28 00:44:04 -0400

    Initialize BareMetalI2C project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <avr/io.h>
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  DDRC |= (1 << PC7); // Set PC7 as output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PINC = (1 << PC7); // Toggle PC7
+  delay(500); // Wait for 500 milliseconds
+  PINC = (1 << PC7); // Toggle PC7 again
+  delay(500); // Wait for 500 milliseconds
+}
